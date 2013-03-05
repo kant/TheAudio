1d4cc476be12ce41748917fcc01c26a8254d6f51
Bailey <bailey.r.smith@gmail.com>
2013-03-05 14:29:12 -0800

    wasn't working, fixed it.

--- custom.php
+++ custom.php
@@ -2,22 +2,22 @@
 
 function get_speaker_playlist()
 {
-$collections=get_records("Collection", array("public"=>"true","featured"=>"true"));
-$current_collection=$collections[0];
-$items=get_records("Item", array("collection"=>$current_collection));
-$playlist = array();
-foreach ($items as $item) {
-    $titleMetadata = metadata($item, array('Dublin Core', 'Title'));
-    foreach ($item->Files as $file) {
-        $sourceMetadata = metadata($file, 'uri');
-        $imageMetadata = metadata($file, 'square_thumbnail_uri');
-        if ($file->getExtension() == 'mp3') {
-            $playlist[] = array(
-                '0'      => array('src' => $sourceMetadata, 'type' => 'audio/mp3'),
-                'config' => array('title' => $titleMetadata, 'poster' => $imageMetadata)
-            );
-        } 
-    }       
+	$collections=get_records("Collection", array("public"=>"true","featured"=>"true"));
+	$current_collection=end($collections);
+	$items=get_records("Item", array("collection"=>$current_collection));
+	$playlist = array();
+	foreach ($items as $item) {
+	    $titleMetadata = metadata($item, array('Dublin Core', 'Title'));
+	    foreach ($item->Files as $file) {
+	        $sourceMetadata = metadata($file, 'uri');
+	        $imageMetadata = metadata($file, 'square_thumbnail_uri');
+	        if ($file->getExtension() == 'mp3') {
+	            $playlist[] = array(
+	                '0' => array('src' => $sourceMetadata, 'type' => 'audio/mp3'),
+	                'config' => array('title' => $titleMetadata, 'poster' => $imageMetadata)
+	            );
+	        }
+	}       
 }
 
 $jsonPlaylist = json_encode($playlist);
